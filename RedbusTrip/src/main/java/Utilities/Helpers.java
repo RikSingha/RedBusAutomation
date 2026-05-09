88012ecdbdb88e49dede9868aace3c10853ca059
Rik Sigha <Rik Sigha@DESKTOP-U5GB0I9>
2026-05-09 18:12:49 +0530

    Added date error fix

--- RedbusTrip/src/main/java/Utilities/Helpers.java
+++ RedbusTrip/src/main/java/Utilities/Helpers.java
@@ -9,13 +9,21 @@ public class Helpers {
 		LocalDate currentDate = LocalDate.now();
 		int currentMonth = currentDate.getMonthValue();
 		int inputmonth = currentMonth+2;
+		String inputDate;
 		if(inputmonth<10) {
 			inputMonth="0"+String.valueOf(inputmonth);
 		}
 		else {
 			inputMonth=String.valueOf(inputmonth);
 		}
-		String newInputDate = String.valueOf(currentDate.getYear())+"-"+inputMonth+"-"+String.valueOf(currentDate.getDayOfMonth());
+		if(currentDate.getDayOfMonth()<10) {
+			inputDate="0"+String.valueOf(currentDate.getDayOfMonth());
+		}
+		else {
+			inputDate=String.valueOf(currentDate.getDayOfMonth());
+		}
+		String newInputDate = String.valueOf(currentDate.getYear())+"-"+inputMonth+"-"+inputDate;
+		System.out.println(newInputDate);
 		return newInputDate;
 	}
 
